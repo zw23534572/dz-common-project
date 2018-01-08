624c8d0c6e61277277251a1690169f29bccb801e
huqichao <qichao.hu@dazong.com>
2018-01-08 10:51:03 +0800

    增加mq测试

---
+++ example/example-common/src/main/java/com/dazong/example/common/constant/MQConstant.java
@@ -0,0 +1,10 @@
+package com.dazong.example.common.constant;
+
+/**
+ * @author huqichao
+ * @date 2018-01-08 10:23
+ */
+public class MQConstant {
+
+    public static final String MQ_DESTINATION = "MQ_TEST";
+}
--- example/example-web/src/main/java/com/dazong/example/web/configurer/ElasticJobConfigurer.java
+++ example/example-web/src/main/java/com/dazong/example/web/configurer/ElasticJobConfigurer.java
@@ -1,9 +1,16 @@
 package com.dazong.example.web.configurer;
 
 
+import com.dangdang.ddframe.job.config.JobCoreConfiguration;
+import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
+import com.dangdang.ddframe.job.lite.api.JobScheduler;
+import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
+import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
 
+import com.dazong.common.mq.job.ReTryNotifyJob;
+import com.dazong.common.mq.job.ReTrySendJob;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,4 +35,19 @@ public class ElasticJobConfigurer {
         return new ZookeeperRegistryCenter(configuration);
     }
 
+    @Bean(initMethod = "init")
+    public JobScheduler registryReTrySendJob(ReTrySendJob reTrySendJob) {
+        LiteJobConfiguration liteJobConfiguration = LiteJobConfiguration.newBuilder(new SimpleJobConfiguration(
+                JobCoreConfiguration.newBuilder(ReTrySendJob.class.getSimpleName(), "0 0/1 * * * ?", 1).build(),
+                ReTrySendJob.class.getCanonicalName())).overwrite(true).build();
+        return new SpringJobScheduler(reTrySendJob, regCenter, liteJobConfiguration);
+    }
+
+    @Bean(initMethod = "init")
+    public JobScheduler registryReTryNotifyJob(ReTryNotifyJob reTryNotifyJob) {
+        LiteJobConfiguration liteJobConfiguration = LiteJobConfiguration.newBuilder(new SimpleJobConfiguration(
+                JobCoreConfiguration.newBuilder(ReTryNotifyJob.class.getSimpleName(), "0 0/5 * * * ?", 1).build(),
+                ReTryNotifyJob.class.getCanonicalName())).overwrite(true).build();
+        return new SpringJobScheduler(reTryNotifyJob, regCenter, liteJobConfiguration);
+    }
 }
