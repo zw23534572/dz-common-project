bae8638f8e513d57c968bae8c55b1d0cde928e0c
FMSCM\lori.li <lori.li@dazong.com>
2018-01-13 15:41:35 +0800

    打开文件UT

--- dz-common-fegin-interface/src/test/java/com/dazong/common/feignclient/client/api/IUploadServiceTest.java
+++ dz-common-fegin-interface/src/test/java/com/dazong/common/feignclient/client/api/IUploadServiceTest.java
@@ -14,12 +14,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Base64Utils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = FeignclientApplication.class)
@@ -30,9 +33,9 @@ public class IUploadServiceTest {
     @Autowired
     private IUploadService upService;
 
-    //        @Test
+    @Test
     public void testUpload() {
-        File file = new File("C:\\Users\\lori.li\\Desktop\\123.jpg");
+        File file = this.createFile();
         String time = String.valueOf(System.currentTimeMillis());
         String configName = "qualitybook";
         //生成数字签名
@@ -44,6 +47,7 @@ public class IUploadServiceTest {
         param.add("config_name", configName);
         param.add("sign", sign);
         DataResponse<String> response = this.upService.upload(param);
+        this.delFile(file);
         if (response.isSuccess()) {
             logger.info("result={}", response);
         }
@@ -53,24 +57,55 @@ public class IUploadServiceTest {
         return DigestUtils.md5Hex("config_name" + "time" + configName + time + "fmdz.2015");
     }
 
-    //        @Test
+    @Test
     public void testUploadUtils1() {
-        File file = new File("C:\\Users\\lori.li\\Desktop\\123.jpg");
+        File file = this.createFile();
         DataResponse<String> response = UploadUtils.upload(file);
-        logger.info("result={}", response);
-    }
-
-    //    @Test
-    public void testUploadUtils2() throws Exception {
-        File file = new File("C:\\Users\\lori.li\\Desktop\\123.jpg");
-        FileInputStream inputStream = new FileInputStream(file);
-        MultipartFile multipartFile = new MockMultipartFile("temp.jpg", inputStream);
-        DataResponse<String> response = UploadUtils.upload(multipartFile);
+        this.delFile(file);
         logger.info("result={}", response);
     }
 
     @Test
-    public void test() {
-        //防止打包报错；上面的test都注释掉了，因为在服务器上文件不存在
+    public void testUploadUtils2() throws Exception {
+        File file = this.createFile();
+        FileInputStream inputStream = new FileInputStream(file);
+        MultipartFile multipartFile = new MockMultipartFile("temp.txt", inputStream);
+        DataResponse<String> response = UploadUtils.upload(multipartFile);
+        this.delFile(file);
+        logger.info("result={}", response);
+    }
+
+    private File createFile() {
+        String context = "Lori test1";
+        String base64 = Base64Utils.encodeToString(context.getBytes());
+        File file = new File("/temp.txt");
+        ByteArrayInputStream in = null;
+        try (FileOutputStream out = new FileOutputStream(file)) {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            byte[] bytes = Base64Utils.decodeFromString(base64);
+            in = new ByteArrayInputStream(bytes);
+            byte[] buffer = new byte[1024];
+            int byteread = 0;
+            while ((byteread = in.read(buffer)) != -1) {
+                out.write(buffer, 0, byteread); // 文件写操作
+            }
+        } catch (Exception e) {
+            logger.error("创建文件失败", e);
+        } finally {
+            if (null != in) {
+                try {
+                    in.close();
+                } catch (Exception e) {
+                    logger.error("关闭输入流异常={}", e);
+                }
+            }
+        }
+        return file;
+    }
+
+    private void delFile(File file) {
+        file.deleteOnExit();
     }
 }
