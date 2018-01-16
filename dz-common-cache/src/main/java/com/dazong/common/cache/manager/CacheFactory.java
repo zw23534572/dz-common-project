99ed7ee212e296088f8ff6edbfa294a6779c533f
李审霖 <shenlin.li@dazong.com>
2018-01-16 17:31:17 +0800

    根据sonar修改代码

--- dz-common-cache/src/main/java/com/dazong/common/cache/manager/CacheFactory.java
+++ dz-common-cache/src/main/java/com/dazong/common/cache/manager/CacheFactory.java
@@ -8,8 +8,6 @@ import com.dazong.common.cache.core.impl.MemcacheHandler;
 import com.dazong.common.cache.core.impl.RedisCacheHandler;
 import com.dazong.common.cache.exception.CacheException;
 import groovy.lang.Singleton;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ApplicationObjectSupport;
