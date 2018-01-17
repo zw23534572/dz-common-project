18ff44e709c5f24007e57d432815761ce853d868
huqichao <qichao.hu@dazong.com>
2018-01-17 15:45:54 +0800

    增加监控

--- dz-common-web/src/main/java/com/dazong/common/web/support/monitor/ThreadDumpServlet.java
+++ dz-common-web/src/main/java/com/dazong/common/web/support/monitor/ThreadDumpServlet.java
@@ -1,5 +1,7 @@
 package com.dazong.common.web.support.monitor;
 
+import com.alibaba.fastjson.JSON;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,9 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
-import java.lang.management.ThreadMXBean;
+import java.lang.management.*;
 
 /**
  * 获取jvm线程堆栈
@@ -27,17 +27,62 @@ public class ThreadDumpServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
-        ThreadInfo[] infos = bean.dumpAllThreads(true, true);
+        String type = req.getParameter("type");
+        ServerInfo serverInfo = new ServerInfo();
 
-        StringBuffer sb = new StringBuffer(102400);
-        for (ThreadInfo info : infos) {
-            sb.append(info);
+        //获取内存信息
+        if ("1".equals(type) || "all".equals(type)) {
+            serverInfo.setJvmArgs(ManagementFactory.getRuntimeMXBean().getInputArguments().toString());
+            getMemory(serverInfo);
         }
+        //获取线程信息
+        if ("2".equals(type) || "all".equals(type)) {
+            getThread(serverInfo);
+        }
+        //获取threadDump信息
+        if ("3".equals(type) || "all".equals(type)){
+            getThreadDump(serverInfo);
+        }
+
         PrintWriter out = resp.getWriter();
-        out.append(sb);
+        out.append(JSON.toJSONString(serverInfo));
 
         out.flush();
         out.close();
+    }
+
+    private void getThreadDump(ServerInfo serverInfo){
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        ThreadInfo[] infos = bean.dumpAllThreads(true, true);
+
+        StringBuilder dumpStr = new StringBuilder(102400);
+        for (ThreadInfo info : infos) {
+            dumpStr.append(info);
+        }
+
+        serverInfo.setThreadDump(dumpStr.toString());
+    }
+
+    private void getMemory(ServerInfo serverInfo){
+        MemoryMXBean memoryMBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage headUsage = memoryMBean.getHeapMemoryUsage();
+        serverInfo.setHeadJvmInit(headUsage.getInit());
+        serverInfo.setHeadJvmUsed(headUsage.getUsed());
+        serverInfo.setHeadJvmCommitted(headUsage.getCommitted());
+        serverInfo.setHeadJvmMax(headUsage.getMax());
+
+        MemoryUsage nonHeadUsage = memoryMBean.getNonHeapMemoryUsage();
+        serverInfo.setNonHeadJvmInit(nonHeadUsage.getInit());
+        serverInfo.setNonHeadJvmUsed(nonHeadUsage.getUsed());
+        serverInfo.setNonHeadJvmCommitted(nonHeadUsage.getCommitted());
+        serverInfo.setNonHeadJvmMax(nonHeadUsage.getMax());
+    }
+
+    private void getThread(ServerInfo serverInfo){
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        serverInfo.setThreadCount(threadMXBean.getThreadCount());
+        serverInfo.setDaemonThreadCount(threadMXBean.getDaemonThreadCount());
+        serverInfo.setPeakThreadCount(threadMXBean.getPeakThreadCount());
+        serverInfo.setTotalStartedThreadCount(threadMXBean.getTotalStartedThreadCount());
     }
 }
