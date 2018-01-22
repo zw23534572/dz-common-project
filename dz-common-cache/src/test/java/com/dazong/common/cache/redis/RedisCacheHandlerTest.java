e1051fb6dd4fc6d79268fa0a56f43de28daea1ad
李审霖 <shenlin.li@dazong.com>
2018-01-22 18:38:27 +0800

    使用Redis内存server运行UT

--- dz-common-cache/src/test/java/com/dazong/common/cache/redis/RedisCacheHandlerTest.java
+++ dz-common-cache/src/test/java/com/dazong/common/cache/redis/RedisCacheHandlerTest.java
@@ -28,7 +28,7 @@ import static junit.framework.TestCase.*;
  */
 @RunWith(SpringRunner.class)
 @ContextConfiguration("/META-INF/dz-common-cache-test.xml")
-public class RedisCacheHandlerTests {
+public class RedisCacheHandlerTest {
 
     @Autowired
     CacheFactory cacheFactory;
