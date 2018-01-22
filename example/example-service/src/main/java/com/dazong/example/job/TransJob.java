f672a1d9ca2057d004d5748c4c9160cd132da3b0
yanghui <hui.yang@dazong.com>
2018-01-22 17:50:43 +0800

    trans的 example

--- example/example-service/src/main/java/com/dazong/example/job/TransJob.java
+++ example/example-service/src/main/java/com/dazong/example/job/TransJob.java
@@ -1,17 +1,14 @@
 package com.dazong.example.job;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import com.dazong.common.elasticjob.annotation.EjTask;
-@EjTask(cron="0/50 * * * * ?")
-public class TestJob implements SimpleJob {
+import com.dazong.common.trans.DzTransactionScheduler;
+@EjTask(cron="0/3 * * * * ?")
+public class TransJob implements SimpleJob{
 
 	@Override
 	public void execute(ShardingContext shardingContext) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println("TestJob：" + sdf.format(new Date()));
+		DzTransactionScheduler.get().scheduleTask();
 	}
 }
