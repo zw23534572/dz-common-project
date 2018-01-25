93efbe6c61bb5d83c124e0447c78f08f84a621eb
luobw <binwen.luo@dazong.com>
2018-01-25 14:48:51 +0800

    增加system_code 的默认值

--- example/example-web/src/main/java/com/dazong/example/web/controller/TransComtroller.java
+++ example/example-web/src/main/java/com/dazong/example/web/controller/TransComtroller.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dazong.example.service.trans.ITransService;
+
 
 @RestController
 @RequestMapping("/trans")
