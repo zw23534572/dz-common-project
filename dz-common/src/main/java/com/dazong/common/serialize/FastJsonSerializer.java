0ce445f5f3741efcefab4f4e2de0bc2f1b70a827
luobw <binwen.luo@dazong.com>
2018-01-29 17:44:34 +0800

    代码优化

--- dz-common/src/main/java/com/dazong/common/serialize/FastJsonSerializer.java
+++ dz-common/src/main/java/com/dazong/common/serialize/FastJsonSerializer.java
@@ -1,36 +1,34 @@
 package com.dazong.common.serialize;
 
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.Feature;
 import com.dazong.common.exceptions.SerializeException;
 
+/**
+ * fastjson序列化
+ * 
+ * @author luobinwen
+ *
+ */
 public class FastJsonSerializer extends AbstractObjectSerializer {
 	private static final String CHARSET = "utf-8";
 
 	@Override
-	public byte[] serialize(Object[] params) {
-		if (params == null) {
-			return new byte[0];
-		}
-
+	public byte[] doSerialize(Object[] params) {
 		try {
 			return JSON.toJSONString(params).getBytes(CHARSET);
 		} catch (Exception e) {
-			throw new SerializeException("json参数序列化不存在编码:" + CHARSET, e);
+			throw new SerializeException("json序列化异常,当前使用编码为:" + CHARSET, e);
 		}
+
 	}
 
 	@Override
-	public Object[] deserialize(byte[] bytes, Class<?>[] clz) {
-		if (bytes == null || clz == null) {
-			return new Object[0];
-		}
-
+	public Object[] doDeserialize(byte[] bytes, Class<?>[] clz) {
 		try {
 			return JSON.parseArray(new String(bytes, CHARSET), clz).toArray(new Object[] {});
 		} catch (Exception e) {
-			throw new SerializeException("json参数反序列化不存在编码:" + CHARSET, e);
+			throw new SerializeException("json反序列化异常,当前使用编码为:" + CHARSET, e);
 		}
 	}
 
@@ -39,7 +37,7 @@ public class FastJsonSerializer extends AbstractObjectSerializer {
 		try {
 			return (T) JSON.parse(new String(bytes, CHARSET), Feature.DisableSpecialKeyDetect);
 		} catch (Exception e) {
-			throw new SerializeException("json参数序列化不存在编码:" + CHARSET, e);
+			throw new SerializeException("json序列化异常,当前使用编码为:" + CHARSET, e);
 		}
 	}
 
@@ -48,7 +46,7 @@ public class FastJsonSerializer extends AbstractObjectSerializer {
 		try {
 			return JSON.toJSONString(object).getBytes(CHARSET);
 		} catch (Exception e) {
-			throw new SerializeException("json参数序列化不存在编码:" + CHARSET, e);
+			throw new SerializeException("json反序列化异常,当前使用编码为:" + CHARSET, e);
 		}
 	}
 
