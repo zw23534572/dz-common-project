abc5fb11eb07414bc4867aea7bda7150421f7ece
yanghui <hui.yang@dazong.com>
2018-02-07 16:29:36 +0800

    去除retryfailEvent序列化接口

--- dz-common-trans/src/main/java/com/dazong/common/trans/listener/RetryFailEvent.java
+++ dz-common-trans/src/main/java/com/dazong/common/trans/listener/RetryFailEvent.java
@@ -1,15 +1,12 @@
 package com.dazong.common.trans.listener;
 
-import java.io.Serializable;
 import java.lang.reflect.Method;
 
 import lombok.Data;
 
 @Data
-public class RetryFailEvent implements Serializable{
+public class RetryFailEvent{
 	
-	private static final long serialVersionUID = 8216662080744121904L;
-
 	private String transactionId;
 	
 	private String transactionName;
