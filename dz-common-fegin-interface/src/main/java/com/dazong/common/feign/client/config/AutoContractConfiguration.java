afdec5539fda5bbeeef1b09633f3116373a03dd6
FMSCM\lori.li <lori.li@dazong.com>
2018-02-08 10:34:59 +0800

    解决坏味道

--- dz-common-fegin-interface/src/main/java/com/dazong/common/feign/client/config/AutoContractConfiguration.java
+++ dz-common-fegin-interface/src/main/java/com/dazong/common/feign/client/config/AutoContractConfiguration.java
@@ -1,7 +1,6 @@
 package com.dazong.common.feign.client.config;
 
 import com.dazong.common.feign.client.api.IContractService;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
--- dz-common-fegin-interface/src/main/java/com/dazong/common/feign/client/config/AutoUploadConfiguration.java
+++ dz-common-fegin-interface/src/main/java/com/dazong/common/feign/client/config/AutoUploadConfiguration.java
@@ -2,7 +2,6 @@ package com.dazong.common.feign.client.config;
 
 import com.dazong.common.feign.client.api.IUploadService;
 import com.dazong.common.feign.client.utils.UploadUtils;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
